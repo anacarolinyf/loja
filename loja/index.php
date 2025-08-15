0746a45c8f853cc4c0f7f2cdad0d1921c32e59b7
anacarolinyf <161509042+anacarolinyf@users.noreply.github.com>
2025-08-15 17:20:58 -0300

    Add files via upload

---
+++ loja/index.php
@@ -0,0 +1,28 @@
+<?php
+  include 'cabecalho.php';
+ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema com CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Bem vindo ao primeiro sistema com CRUD</h1>
+        <h2>Ana Caroliny Fortes & Heloisa Lima Rodrigues</h2>
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+             <a href="#" type="button " class="btn btn-danger">Left</a>
+             <a href="#" type="button" class="btn btn-warning">Middle</a>
+             
+</div>
+        
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
