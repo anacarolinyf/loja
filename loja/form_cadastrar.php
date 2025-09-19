828080cc8d93b0b4bef2fc3c511643bd85298f41
anacarolinyf <161509042+anacarolinyf@users.noreply.github.com>
2025-09-19 16:49:00 -0300

    Add files via upload

--- loja/form_cadastrar.php
+++ loja/form_cadastrar.php
@@ -14,10 +14,15 @@
             <div class="mb-3">
                 <input type="text" name="quantidade" class="form-control" placeholder="Digite a quantidade do produto">
             </div>
-            <button type="submit" class="btn btn-danger">Cadastrar</button>
-        </form>
-        
+            <button type="submit" class="btn btn-light">Cadastrar</button>
+        </form> 
     </div>
+
+    <style>
+        body{
+            background-color: #FFE4E1;
+        }
+    </style>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous">
     </script>
