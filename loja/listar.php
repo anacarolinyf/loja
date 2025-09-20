53208dbaf9623b6b597d6375b41bf267e27fea35
anacarolinyf <161509042+anacarolinyf@users.noreply.github.com>
2025-09-20 12:43:49 -0300

    Add files via upload

---
+++ loja/listar.php
@@ -0,0 +1,58 @@
+<?php
+include 'cabecalho.php';
+?>
+<body>
+
+  <style>
+    body{
+        background-color: #ffe6eb;
+    }
+  </style>
+
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">NOME</th>
+                    <th scope="col">PREÇO</th>
+                    <th scope="col">QUANTIDADE</th>
+                    <th scope="col">OPÇÔES</th>
+            </tr>
+         </thead>
+        <tbody>
+
+            <?php
+                require 'conexao.php';
+                $sql = "SELECT * FROM produtos";
+                $stmt = $pdo->query($sql);
+                while ($produto = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    echo "<tr>";
+                    echo "<td>".$produto['id']."</td>";
+                    echo "<td>". $produto['nome']."</td>";
+                    echo "<td>". $produto['preco']."</td>";
+                    echo "<td>". $produto['quantidade']."</td>";
+                    echo "<td>
+                          <div class='btn-group' role='group'>
+                        <a href='form_atualizar.php' type='button' class='btn btn-light'>Atualizar</a>
+                        <a href='#' type='button' class='btn btn-dark'>Apagar</a>
+                        </div>
+                    </td>
+                    "; 
+                    echo "<tr>";
+
+
+                }
+         ?>
+
+
+
+
+
+
+         
+          </tbody>
+        </table>
+        </div>
+     </body>
+   </html>
